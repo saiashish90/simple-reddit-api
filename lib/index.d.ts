dc196be175f4b8ebfe32192cab6eac1bd9e9b052
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-12-27 23:29:14 +0530

    Npm changes

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface Options {
-    subreddit?: string;
-    count?: number;
-    is_meme?: boolean;
-    fulldata?: boolean;
-}
-export declare function topPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
-export {};
-//# sourceMappingURL=index.d.ts.map
