ebb37c1bf381f262c8c244d507edede19890a489
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-12-27 23:15:08 +0530

    Built new version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,9 @@
 interface Options {
     subreddit?: string;
+    count?: number;
     is_meme?: boolean;
     fulldata?: boolean;
 }
-export declare function topPost({ subreddit, is_meme, fulldata }?: Options): Promise<any>;
+export declare function topPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
 export {};
 //# sourceMappingURL=index.d.ts.map
