64975643f533318d64c555633e05342d1a93f7a9
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-12-27 21:18:09 +0530

    Can get n number of top posts

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+interface Options {
+    subreddit?: string;
+    is_meme?: boolean;
+    fulldata?: boolean;
+}
+export declare function topPost({ subreddit, is_meme, fulldata }?: Options): Promise<any>;
+export {};
+//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 import fetch from 'node-fetch'
 interface Options{
     subreddit?:string,
+    count?:number,
     is_meme?:boolean,
     fulldata?:boolean
 }
-export async function topPost({subreddit='all',is_meme=false,fulldata=false}:Options={}){
+interface Postessentials{
+    data:{
+        title:string,
+        author:string,
+        subreddit_name_prefixed:string, 
+        ups:number,total_awards_received:number,
+        url:string
+    }
+}
+export async function topPost({subreddit='all',count=1,is_meme=false,fulldata=false}:Options={}){
     const api:string = 'https://www.reddit.com/r/'
     if(is_meme) subreddit = 'memes'
     try{
-    const response = await fetch(`${api}${subreddit}.json?limit=10`)
+    const response = await fetch(`${api}${subreddit}.json?limit=100`)
     const json = await response.json()
     const posts = json.data.children.sort((a:any,b:any)=>(a.data.ups>b.data.ups)?-1:(a.data.ups<b.data.ups)?1:0)
     if(fulldata){
-        const post = posts[0]
+        const post = posts.slice(0,count)
         return post;
     } 
     else{
-        const post = (({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}})=>({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}))(posts[0])
-        return post
+        const func = (({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}:Postessentials)=>({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}))
+        const newposts = posts.slice(0,count).map(func)
+        return newposts
     }
     }
     catch(error){
         return "subreddit doesnt exist"   
     }
 } 
-topPost({subreddit:'dankmemes',is_meme:true,fulldata:false}).then(console.log)
+
 
