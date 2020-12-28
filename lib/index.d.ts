d226ec024bd888611f76060782d30ad0b5f8d9cb
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-12-28 20:17:31 +0530

    fixed parameter bug

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,21 +1,12 @@
-export declare function topPost(options?: {
-    subreddit: string;
-    count: number;
-    is_meme: boolean;
-    fulldata: boolean;
-}): Promise<any>;
-export declare function newPost(options?: {
-    subreddit: string;
-    count: number;
-    is_meme: boolean;
-    fulldata: boolean;
-}): Promise<any>;
-export declare function randomPost(options?: {
-    subreddit: string;
-    count: number;
-    is_meme: boolean;
-    fulldata: boolean;
-}): Promise<any>;
+interface Options {
+    subreddit?: string;
+    count?: number;
+    is_meme?: boolean;
+    fulldata?: boolean;
+}
+export declare function topPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
+export declare function newPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
+export declare function randomPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
 export declare function searchSubreddits(subreddit: string): Promise<{
     subreddit: string;
     url: string;
@@ -24,4 +15,5 @@ export declare function searchSubreddits(subreddit: string): Promise<{
     url: undefined;
 }>;
 export declare function popularSubreddits(count?: number): Promise<any>;
+export {};
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import fetch from 'node-fetch'
+interface Options{
+    subreddit?:string,
+    count?:number,
+    is_meme?:boolean,
+    fulldata?:boolean
+}
 interface Subreddit{
     data:
         {
@@ -17,20 +23,20 @@ interface Postessentials{
         url:string
     }
 }
-export async function topPost(options={subreddit:'all',count:1,is_meme:false,fulldata:false}){
+export async function topPost({subreddit='all',count=1,is_meme=false,fulldata=false}:Options={}){
     const api:string = 'https://www.reddit.com/r/'
-    if(options.is_meme) options.subreddit = 'memes'
+    if(is_meme) subreddit = 'memes'
     try{
-    const response = await fetch(`${api}${options.subreddit}/top.json?limit=100`)
+    const response = await fetch(`${api}${subreddit}/top.json?limit=100`)
     const json = await response.json()
     const posts = json.data.children
-    if(options.fulldata){
-        const post = posts.slice(0,options.count)
+    if(fulldata){
+        const post = posts.slice(0,count)
         return post;
     } 
     else{
         const func = (({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}:Postessentials)=>({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}))
-        const newposts = posts.slice(0,options.count).map(func)
+        const newposts = posts.slice(0,count).map(func)
         return newposts
     }
     }
@@ -38,20 +44,20 @@ export async function topPost(options={subreddit:'all',count:1,is_meme:false,ful
         return "subreddit doesnt exist"   
     }
 } 
-export async function newPost(options={subreddit:'all',count:1,is_meme:false,fulldata:false}){
+export async function newPost({subreddit='all',count=1,is_meme=false,fulldata=false}:Options={}){
     const api:string = 'https://www.reddit.com/r/'
-    if(options.is_meme) options.subreddit = 'memes'
+    if(is_meme) subreddit = 'memes'
     try{
-    const response = await fetch(`${api}${options.subreddit}/new.json?limit=100`)
+    const response = await fetch(`${api}${subreddit}/new.json?limit=100`)
     const json = await response.json()
     const posts = json.data.children
-    if(options.fulldata){
-        const post = posts.slice(0,options.count)
-        return post;
-    } 
+    if(fulldata){
+        const post = posts.slice(0,count)
+        return post
+    }
     else{
         const func = (({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}:Postessentials)=>({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}))
-        const newposts = posts.slice(0,options.count).map(func)
+        const newposts = posts.slice(0,count).map(func)
         return newposts
     }
     }
@@ -59,21 +65,21 @@ export async function newPost(options={subreddit:'all',count:1,is_meme:false,ful
         return "subreddit doesnt exist"   
     }
 } 
-export async function randomPost(options={subreddit:'all',count:1,is_meme:false,fulldata:false}){
+export async function randomPost({subreddit='all',count=1,is_meme=false,fulldata=false}:Options={}){
     const api:string = 'https://www.reddit.com/r/'
-    if(options.is_meme) options.subreddit = 'memes'
+    if(is_meme) subreddit = 'memes'
     try{
-    const response = await fetch(`${api}${options.subreddit}.json?limit=100`)
+    const response = await fetch(`${api}${subreddit}.json?limit=100`)
     const json = await response.json()
     const posts = json.data.children
     let shuffled = posts.map((a:any) => ({sort: Math.random(), value: a})).sort((a:any, b:any) => a.sort - b.sort).map((a:any) => a.value)
-    if(options.fulldata){
-        const post = shuffled.slice(0,options.count)
+    if(fulldata){
+        const post = shuffled.slice(0,count)
         return post;
     } 
     else{
         const func = (({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}:Postessentials)=>({data:{title,author,subreddit_name_prefixed, ups,total_awards_received,url}}))
-        const newposts = shuffled.slice(0,options.count).map(func)
+        const newposts = shuffled.slice(0,count).map(func)
         return newposts
     }
     }
@@ -103,3 +109,4 @@ export async function popularSubreddits(count:number = 1) {
     const subreddits = json.data.children.slice(0,count).map(func).map((i: Subreddit)=>({data:{...i.data,url:`https://www.reddit.com/${i.data.display_name_prefixed}`}}))
     return subreddits;
 }
+topPost({subreddit:"dankmemes"}).then(console.log)
