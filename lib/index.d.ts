67fc5590dcff2f8e12a5ac00fb4d74b748c638e1
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-12-29 11:08:28 +0530

    Built version xd

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,16 +4,30 @@ interface Options {
     is_meme?: boolean;
     fulldata?: boolean;
 }
-export declare function topPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
-export declare function newPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
-export declare function randomPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<any>;
+export declare function topPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<{
+    status: number;
+    posts: any;
+}>;
+export declare function newPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<{
+    status: number;
+    posts: any;
+}>;
+export declare function randomPost({ subreddit, count, is_meme, fulldata }?: Options): Promise<{
+    status: number;
+    posts: any;
+}>;
 export declare function searchSubreddits(subreddit: string): Promise<{
+    status: number;
     subreddit: string;
     url: string;
 } | {
+    status: number;
     subreddit: undefined;
     url: undefined;
 }>;
-export declare function popularSubreddits(count?: number): Promise<any>;
+export declare function popularSubreddits(count?: number): Promise<{
+    status: number;
+    subreddits: any;
+}>;
 export {};
 //# sourceMappingURL=index.d.ts.map
